b2e179f7c559366a82ba7fed41f31b1423b7fcc0
Ronald Pitts <ronpitts81@gmail.com>
2020-09-13 15:10:52 -0400

    adding db and config

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devour) VALUES ('chicken', true);
+INSERT INTO burgers (burger_name, devour) VALUES ('veggie', true);
+INSERT INTO burgers (burger_name, devour) VALUES ('cheese', true);
