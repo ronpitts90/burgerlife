10e993e1f3a282f13d74d0cd986d8f93339b089d
Ronald Pitts <ronpitts81@gmail.com>
2020-09-15 13:50:02 -0400

    new config

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,37 @@
+
 // Set up MySQL connection.
-var mysql = require("mysql");
-
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3000,
-  user: "root",
-  password: "",
-  database: "bugers_db"
-});
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
+const mysql = require("mysql");
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "scorpion1990",
+    database: "burgers_db"
+  });
+}
+connection.connect();
 // Exporting connection. 
 module.exports = connection;
+
+
+
+// var mysql = require("mysql");
+
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3000,
+//   user: "root",
+//   password: "scorpion1990",
+//   database: "burgers_db"
+// });
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
+
 
