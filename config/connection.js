c7c27ffd30f3a81fa79cd5381e28182ba34bedb6
Ronald Pitts <ronpitts81@gmail.com>
2020-09-15 00:08:29 -0400

    correction

--- config/connection.js
+++ config/connection.js
@@ -3,10 +3,17 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
   host: "localhost",
-  port: 8080,
+  port: 3000,
   user: "root",
   password: "",
   database: "bugers_db"
+});
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
 });
 
 // Exporting connection. 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,5 @@
+USE burgers_db; 
+
 INSERT INTO burgers (burger_name, devour) VALUES ('chicken', true);
 INSERT INTO burgers (burger_name, devour) VALUES ('veggie', true);
-INSERT INTO burgers (burger_name, devour) VALUES ('cheese', true);
+INSERT INTO burgers (burger_name, devour) VALUES ('cheese', true); 
