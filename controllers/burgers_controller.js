5b7541787192deaa319794dea8e873f877d11d94
Ronald Pitts <ronpitts81@gmail.com>
2020-09-15 01:57:33 -0400

    updated controls

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,34 +3,32 @@ var router = express.Router();
 var burger = require("../models/burger.js"); 
 
 router.get("/", function(req, res) {
-    cat.all(function(data) {
+    burger.all(function(data) {
       var hbsObject = {
-        cats: data
+        burgers: data 
       };
       console.log(hbsObject);
       res.render("index", hbsObject);
     });
-  });
+  });   
   
-  router.post("/api/cats", function(req, res) {
-    cat.create([
-      "name", "sleepy"
+  router.post("/api/burgers", function(req, res) {
+    burger.create([
+      "burger_name", "devour"
     ], [
-      req.body.name, req.body.sleepy
+      req.body.burger_name, req.body.devour
     ], function(result) {
       // Send back the ID of the new quote
       res.json({ id: result.insertId });
     });
   });
   
-  router.put("/api/cats/:id", function(req, res) {
+  router.put("/api/burgers/:id", function(req, res) {
     var condition = "id = " + req.params.id;
   
     console.log("condition", condition);
   
-    cat.update({
-      sleepy: req.body.sleepy
-    }, condition, function(result) {
+    burger.update({ devour: req.body.devour}, condition, function(result) {
       if (result.changedRows == 0) {
         // If no rows were changed, then the ID must not exist, so 404
         return res.status(404).end();
@@ -40,7 +38,17 @@ router.get("/", function(req, res) {
     });
   });
 
+router.delete("/api/burgers/:id", function(req, res){
+    var condition = "id =" + req.params.id;
+    console.log("condition", condition); 
 
-
+    burger.delete(condition , function(result){
+        if (result.changedRows === 0) {
+            return res.status(404).end();
+          } else {
+            res.status(200).end();
+          }
+      }); 
+    }); 
 
 module.exports = router; 
