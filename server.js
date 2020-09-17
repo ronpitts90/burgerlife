fe2da6a501885182b60abe2616ad6d80cc897050
Ronald Pitts <ronpitts81@gmail.com>
2020-09-17 14:33:28 -0400

    new files

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require("express");
 var exphbs = require("express-handlebars"); 
 
+
 var app = express(); 
 var PORT = process.env.PORT || 3000; 
+
+app.use(express.static("public"));
 
 app.engine("handlebars", exphbs({defaultLayout: "main"}));
 app.set("view engine" , "handlebars"); 
